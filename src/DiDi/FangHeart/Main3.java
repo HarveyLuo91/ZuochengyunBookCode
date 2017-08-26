ed3936761e33582da2cb095685780aca9b61d881
fangheart <397403564@qq.com>
2017-08-26 14:38:28 +0800

    fang add Main

--- src/DiDi/FangHeart/Main3.java
+++ src/DiDi/FangHeart/Main3.java
@@ -1,10 +1,12 @@
 package DiDi.FangHeart;
 
+import java.util.Scanner;
+
 /**
  * Created by fangheart on 2017/8/26.
  */
 public class Main3 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
     }
 }
