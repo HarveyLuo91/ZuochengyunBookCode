d38ad7a5812613c0a8f3c7bdfd689f8d0d9c6506
Jerry <wangwj1203962899@gmail.com>
2017-09-02 15:49:47 +0800

    pingduoduo

---
+++ src/SouHu/jerry/Test.java
@@ -0,0 +1,12 @@
+package SouHu.jerry;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by jerry on 2017/8/31.
+ */
+public class Test {
+    public static void main(String[] args) {
+        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+    }
+}
